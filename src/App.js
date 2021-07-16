bc6303afe41403110628f4afd493c04623d93e88
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-07-15 17:39:12 -0700

    sc

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import Home from './pages/Home';
 import Episodes from './pages/Episodes';
 import Title from './pages/Title';
+import Sidebar from './pages/Sidebar';
 // import './styles/home.css';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
-      <Title />
-     <Home />
-     <Episodes />
+      <Sidebar />
+      <div className="other-pages">
+        <Title />
+        <Home />
+        <Episodes />
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 import '../styles/home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="header-container">
-      <img className="header-image" src="https://res.cloudinary.com/fleetnation/image/private/c_fit,w_1120/g_south,l_text:style_gothic2:%C2%A9%20Asier%20Romero,o_20,y_10/g_center,l_watermark4,o_25,y_50/v1469550122/zxrk1ld7jme7td4ziwqg.jpg" alt="soul" />
-        <div className="header-titles-container">
-          <p className="header-episode">Podcast Episode</p>
-          <p className="header-title">This is The Podcast Title/Name</p>
-          <p className="header-subtitle">This is the subtitle</p>
+    <div>
+      <div className="home-container">
+        <div className="header-container">
+        <img className="header-image" src="https://res.cloudinary.com/fleetnation/image/private/c_fit,w_1120/g_south,l_text:style_gothic2:%C2%A9%20Asier%20Romero,o_20,y_10/g_center,l_watermark4,o_25,y_50/v1469550122/zxrk1ld7jme7td4ziwqg.jpg" alt="soul" />
+          <div className="header-titles-container">
+            <p className="header-episode">Podcast Episode</p>
+            <p className="header-title">This is The Podcast Title/Name</p>
+            <p className="header-subtitle">This is the subtitle</p>
+          </div>
         </div>
-      </div>
 
-      <div className="episode-description-container">
-        <p className="header-subtitle">Episode Description</p>
-        <p className="episode-description">asdfadsfadsfad sfadsfa
-        dsfadsfasdfa dsfa
-        sdfadsfadfa dsfadfadfadsf
-        adfasdfasdf adsfadsfadfadsfadsfadfadsf
-        adfadsfadf adsfadsfadsfadsfadsfadsf adsfadsfadsfadfadsf
-        adsfadsfa dsfasdfasdfads fadsfadsfa dsfasdfadsfadsfadsf
-        asdfdasd fadsfasdfadsfad sfadfasdfadsfadsfadsfas dfasdfasdf
-        adsfadsfad sfadsfadsfadsfasd fasdfasdfadsfasdf asdfasdfasdf</p>
-      </div>
+        <div className="episode-description-container">
+          <p className="header-subtitle">Episode Description</p>
+          <p className="episode-description">asdfadsfadsfad sfadsfa
+          dsfadsfasdfa dsfa
+          sdfadsfadfa dsfadfadfadsf
+          adfasdfasdf adsfadsfadfadsfadsfadfadsf
+          adfadsfadf adsfadsfadsfadsfadsfadsf adsfadsfadsfadfadsf
+          adsfadsfa dsfasdfasdfads fadsfadsfa dsfasdfadsfadsfadsf
+          asdfdasd fadsfasdfadsfad sfadfasdfadsfadsfadsfas dfasdfasdf
+          adsfadsfad sfadsfadsfadsfasd fasdfasdfadsfasdf asdfasdfasdf</p>
+        </div>
 
+      </div>
     </div>
   )
 }
