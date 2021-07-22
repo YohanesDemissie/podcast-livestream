daa830efc29a6519fa845fe4a984147365ca1f57
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-07-22 16:42:50 -0700

    added reponsive styling to admin login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect} from 'react';
 import fire from '../firebase/config';
+import '../styles/admin.css'
 
 const Login = (props) => {
   const {email, password, setEmail, setPassword, handleLogin, setHasAccount, emailError, passwordError} = props;
   return (
-    <div>
-        <label>Admin login</label>
-        <input type="text" autoFocus value={email} onChange={(e) => setEmail(e.target.value)} id="admin name" placeholder="admin ID" />
+    <div className="admin-form-container">
+      <h1 className="admin-title">Administrative User Only</h1>
+      <form className="admin-form">
+        <label className="admin-label">Admin login</label>
+        <input className="admin-input"type="text" autoFocus value={email} onChange={(e) => setEmail(e.target.value)} id="admin name" placeholder="admin ID" />
         <p>{emailError}</p>
-        <label>Password</label>
-        <input name="admin password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="admin password" placeholder="admin password" />
+        <label className="admin-label">Password</label>
+        <input className="admin-input" name="admin password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="admin password" placeholder="admin password" />
         <p>{passwordError}</p>
-
-
-        <button type="submit" onClick={handleLogin}>Login</button>
+        <button className="admin-submit" type="submit" onClick={handleLogin}>Login</button>
+      </form>
     </div>
   )
 }
