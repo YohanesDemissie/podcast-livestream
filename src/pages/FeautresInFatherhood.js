ea15ee597fb9d726bf78484b0426133b89127024
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-07-28 19:39:31 -0700

    added all dynamic rendering for guest lists, need to make admin form for guest

--- src/pages/FeautresInFatherhood.js
+++ src/pages/FeautresInFatherhood.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const FeaturesInFatherhood = () => {
+  const [posts, setPosts] = useState([]);
   return (
     <div></div>
   )
--- src/pages/GuestStars.js
+++ src/pages/GuestStars.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase/config.js';
+import '../styles/guestStars.css'
+
 
 const GuestStars = () => {
+  const [guests, setGuests] = useState([]);
+
+  useEffect(() => {
+    const getGuestsFromFirebase = [];
+    const subscriber = db.collection('honorableMentions')
+.orderBy("name", "desc").onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        getGuestsFromFirebase.push({...doc.data(), key: doc.id,});
+      });
+      setGuests(getGuestsFromFirebase);
+    })
+    //firebase returns a cleanup function with the line below
+    return () => subscriber();
+
+  }, []); //empty array so useEffect is only called once
   return (
-    <div></div>
+    <div>
+      {guests.length > 0 ? (
+        guests.map((guest) =>
+
+        <div className="episodes-container" key={guest.key}>
+          <div className="episode-container">
+          <h1>{guest.name}</h1>
+          <img src={guest.img} alt="guest-portrait"/>
+            <p>{guest.occupation}</p>
+            <p>{guest.bio}</p>
+            <p>Contact {guest.name} here at {guest.social}</p>
+          </div>
+        </div>
+      )) : (<h1>Nothing Yet</h1>)}
+    </div>
   )
 }
 
