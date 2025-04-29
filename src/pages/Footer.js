06b2e7f9c0577de0c2e2edf40c896c9d5cab38b4
Yohanes Demissie <Yohanesdemissie@gmail.com>
2025-04-28 17:14:16 -0700

    updated firebase import

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -15,12 +15,12 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <a href="https://www.linkedin.com/in/hatch1/"><img className="footer-icon" src="https://www.pinclipart.com/picdir/big/119-1198440_linkedin-logo-and-link-to-tpd-page-linked.png" /></a>
-      <a href="https://www.facebook.com/hatch1"><img className="footer-icon" src={FacebookIcon} /></a>
-      <a href="https://twitter.com/hatch1?lang=en"><img className="footer-icon" src={TwitterIcon} /></a>
-      <a href="https://www.instagram.com/1hatch1/"><img className="footer-icon" src={InstagramIcon} /></a>
+      <a href="https://www.linkedin.com/in/hatch1/"><img className="footer-icon" src="https://www.pinclipart.com/picdir/big/119-1198440_linkedin-logo-and-link-to-tpd-page-linked.png" alt="LinkedIn Logo"/></a>
+      <a href="https://www.facebook.com/hatch1"><img className="footer-icon" src={FacebookIcon} alt="FaceBook Logo"/></a>
+      <a href="https://twitter.com/hatch1?lang=en"><img className="footer-icon" src={TwitterIcon} alt="Twitter Logo"/></a>
+      <a href="https://www.instagram.com/1hatch1/"><img className="footer-icon" src={InstagramIcon} alt="Instagram Logo"/></a>
       {/* <a href=""><img className="footer-icon" src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/youtube-2-icon-18-256.png" /></a> */}
-      <a href=""><img className="footer-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWTI3A8ViRDZkKWwcdEk5_UHiOX3EZYYoGFmRj8oJwrNSBR_KaUuj9wJUGlxAIhbH3E7w&usqp=CAU" /></a> 
+      <a href="https://www.youtube.com/@markhatcher4810"><img className="footer-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWTI3A8ViRDZkKWwcdEk5_UHiOX3EZYYoGFmRj8oJwrNSBR_KaUuj9wJUGlxAIhbH3E7w&usqp=CAU" alt="YouTube Logo"/></a> 
     </footer>
   )
 }
